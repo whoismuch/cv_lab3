4a39e70d45b0c7f6283e4eaee1af11aa8c2e454a
DanishevskiyNS <danishevskiy.ns@gmail.com>
2023-10-21 12:48:53 +0300

    PIL Image in dataset.py

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+from PIL import Image
 
 def generate_annotations_file(img_dir='datasets', annots_save_path='annotation_file.csv'):
     '''
@@ -46,7 +47,7 @@ class FlowersDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
+        image = Image.open(img_path)
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
